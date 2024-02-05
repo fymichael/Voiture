f9e9af83265e4216e791c67e26c9ac3d1c340ac3
fymichael <118510462+fymichael@users.noreply.github.com>
2024-02-05 11:19:27 +0300

    de

--- src/main/java/com/project/Voiture/model/connection/Connect.java
+++ src/main/java/com/project/Voiture/model/connection/Connect.java
@@ -8,6 +8,7 @@ public class Connect {
     public static Connection connectDB() throws Exception
     {
         Class.forName("org.postgresql.Driver");
+        
         Connection con = DriverManager.getConnection("jdbc:postgresql://viaduct.proxy.rlwy.net:32444/railway", "postgres", "41d5e*5G1*B42D6CgDe4fbc2CA3Eba*3");
         return con;
     }
